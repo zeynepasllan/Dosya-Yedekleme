819c8e1e327a8ae26d5385c249441ce1c3a153b7
zeynepasllan <90160240+zeynepasllan@users.noreply.github.com>
2022-10-07 14:42:24 +0300

    Dosya-Yedekleme

---
+++ Dosya_Yedekleme/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace Dosya_Yedekleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnKaynak = new System.Windows.Forms.Button();
+            this.btnHedef = new System.Windows.Forms.Button();
+            this.btnYedekle = new System.Windows.Forms.Button();
+            this.textBoxKaynak = new System.Windows.Forms.TextBox();
+            this.textBoxHedef = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBoxDosyalar = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1_Sil_GeriAl = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geriAlStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblSilinen = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBoxSil = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.listBoxSilinenler = new System.Windows.Forms.ListBox();
+            this.btnZiple = new System.Windows.Forms.Button();
+            this.contextMenuStrip1_Sil_GeriAl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnKaynak
+            // 
+            this.btnKaynak.Location = new System.Drawing.Point(87, 64);
+            this.btnKaynak.Name = "btnKaynak";
+            this.btnKaynak.Size = new System.Drawing.Size(75, 23);
+            this.btnKaynak.TabIndex = 0;
+            this.btnKaynak.Text = "Kaynak";
+            this.btnKaynak.UseVisualStyleBackColor = true;
+            this.btnKaynak.Click += new System.EventHandler(this.btnKaynak_Click);
+            // 
+            // btnHedef
+            // 
+            this.btnHedef.Location = new System.Drawing.Point(87, 114);
+            this.btnHedef.Name = "btnHedef";
+            this.btnHedef.Size = new System.Drawing.Size(75, 23);
+            this.btnHedef.TabIndex = 1;
+            this.btnHedef.Text = "Hedef";
+            this.btnHedef.UseVisualStyleBackColor = true;
+            this.btnHedef.Click += new System.EventHandler(this.btnHedef_Click);
+            // 
+            // btnYedekle
+            // 
+            this.btnYedekle.Location = new System.Drawing.Point(409, 567);
+            this.btnYedekle.Name = "btnYedekle";
+            this.btnYedekle.Size = new System.Drawing.Size(118, 23);
+            this.btnYedekle.TabIndex = 2;
+            this.btnYedekle.Text = "Yedekle";
+            this.btnYedekle.UseVisualStyleBackColor = true;
+            this.btnYedekle.Click += new System.EventHandler(this.btnYedekle_Click);
+            // 
+            // textBoxKaynak
+            // 
+            this.textBoxKaynak.Location = new System.Drawing.Point(208, 64);
+            this.textBoxKaynak.Name = "textBoxKaynak";
+            this.textBoxKaynak.Size = new System.Drawing.Size(302, 23);
+            this.textBoxKaynak.TabIndex = 3;
+            // 
+            // textBoxHedef
+            // 
+            this.textBoxHedef.Location = new System.Drawing.Point(208, 115);
+            this.textBoxHedef.Name = "textBoxHedef";
+            this.textBoxHedef.Size = new System.Drawing.Size(302, 23);
+            this.textBoxHedef.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBoxDosyalar
+            // 
+            this.listBoxDosyalar.ContextMenuStrip = this.contextMenuStrip1_Sil_GeriAl;
+            this.listBoxDosyalar.FormattingEnabled = true;
+            this.listBoxDosyalar.ItemHeight = 15;
+            this.listBoxDosyalar.Location = new System.Drawing.Point(382, 275);
+            this.listBoxDosyalar.Name = "listBoxDosyalar";
+            this.listBoxDosyalar.ScrollAlwaysVisible = true;
+            this.listBoxDosyalar.Size = new System.Drawing.Size(297, 274);
+            this.listBoxDosyalar.TabIndex = 5;
+            // 
+            // contextMenuStrip1_Sil_GeriAl
+            // 
+            this.contextMenuStrip1_Sil_GeriAl.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.silToolStripMenuItem,
+            this.geriAlStripMenuItem1});
+            this.contextMenuStrip1_Sil_GeriAl.Name = "contextMenuStrip1";
+            this.contextMenuStrip1_Sil_GeriAl.Size = new System.Drawing.Size(110, 48);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // geriAlStripMenuItem1
+            // 
+            this.geriAlStripMenuItem1.Name = "geriAlStripMenuItem1";
+            this.geriAlStripMenuItem1.Size = new System.Drawing.Size(109, 22);
+            this.geriAlStripMenuItem1.Text = "Geri Al";
+            this.geriAlStripMenuItem1.Click += new System.EventHandler(this.geriAlStripMenuItem1_Click);
+            // 
+            // lblSilinen
+            // 
+            this.lblSilinen.AutoSize = true;
+            this.lblSilinen.Location = new System.Drawing.Point(41, 272);
+            this.lblSilinen.Name = "lblSilinen";
+            this.lblSilinen.Size = new System.Drawing.Size(0, 15);
+            this.lblSilinen.TabIndex = 8;
+            this.lblSilinen.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(655, 567);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(24, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // textBoxSil
+            // 
+            this.textBoxSil.Location = new System.Drawing.Point(47, 234);
+            this.textBoxSil.Name = "textBoxSil";
+            this.textBoxSil.Size = new System.Drawing.Size(215, 23);
+            this.textBoxSil.TabIndex = 10;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(268, 234);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 11;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // listBoxSilinenler
+            // 
+            this.listBoxSilinenler.FormattingEnabled = true;
+            this.listBoxSilinenler.ItemHeight = 15;
+            this.listBoxSilinenler.Location = new System.Drawing.Point(41, 275);
+            this.listBoxSilinenler.Name = "listBoxSilinenler";
+            this.listBoxSilinenler.ScrollAlwaysVisible = true;
+            this.listBoxSilinenler.Size = new System.Drawing.Size(302, 274);
+            this.listBoxSilinenler.TabIndex = 12;
+            // 
+            // btnZiple
+            // 
+            this.btnZiple.Location = new System.Drawing.Point(531, 567);
+            this.btnZiple.Name = "btnZiple";
+            this.btnZiple.Size = new System.Drawing.Size(118, 23);
+            this.btnZiple.TabIndex = 13;
+            this.btnZiple.Text = "Ziple";
+            this.btnZiple.UseVisualStyleBackColor = true;
+            this.btnZiple.Click += new System.EventHandler(this.btnZiple_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 624);
+            this.Controls.Add(this.btnZiple);
+            this.Controls.Add(this.listBoxSilinenler);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBoxSil);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblSilinen);
+            this.Controls.Add(this.listBoxDosyalar);
+            this.Controls.Add(this.textBoxHedef);
+            this.Controls.Add(this.textBoxKaynak);
+            this.Controls.Add(this.btnYedekle);
+            this.Controls.Add(this.btnHedef);
+            this.Controls.Add(this.btnKaynak);
+            this.Name = "Form1";
+            this.Text = "Dosya Yedekle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1_Sil_GeriAl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaynak;
+        private System.Windows.Forms.Button btnHedef;
+        private System.Windows.Forms.Button btnYedekle;
+        private System.Windows.Forms.TextBox textBoxKaynak;
+        private System.Windows.Forms.TextBox textBoxHedef;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBoxDosyalar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1_Sil_GeriAl;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem geriAlStripMenuItem1;
+        private System.Windows.Forms.Label lblSilinen;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBoxSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox listBoxSilinenler;
+        private System.Windows.Forms.Button btnZiple;
+    }
+}
+
---
+++ Dosya_Yedekleme/Form1.cs
@@ -0,0 +1,186 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dosya_Yedekleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxKaynak.Enabled = false;
+            textBoxHedef.Enabled = false;
+        }
+
+        private void btnKaynak_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Yedeklenecek klasörü seçiniz.";
+            openFileDialog1.FileName = "";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                kaynakKlasor = folderBrowserDialog1.SelectedPath.ToString();
+                textBoxKaynak.Text = kaynakKlasor;
+                listBoxDosyalar.Items.Clear();
+                var dosyalar = new DirectoryInfo(kaynakKlasor).GetFiles("*.*");
+                foreach (FileInfo dosya in dosyalar)
+                {
+                    listBoxDosyalar.Items.Add(dosya.Name);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Klasör seçmediniz.");
+            }
+        }
+
+        private void btnHedef_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                hedefKlasor = folderBrowserDialog1.SelectedPath.ToString();
+                textBoxHedef.Text = hedefKlasor;
+            }
+            else
+            {
+                MessageBox.Show("Hedef klasörü seçmediniz.");
+            }
+        }
+
+        private void btnYedekle_Click(object sender, EventArgs e)
+        {
+            if (hedefKlasor != "" && kaynakKlasor != "")
+            {
+
+                int sayac = 0;
+                foreach (string item in listBoxDosyalar.Items)
+                {
+                    if (!File.Exists(hedefKlasor + "\\" + item))
+                    {
+                        File.Copy(kaynakKlasor + "\\" + item, hedefKlasor + "\\" + item);
+                        sayac++;
+                    }
+                }
+                MessageBox.Show($"{sayac} Adet Dosya Yedekleme İşlemi Başarılı");
+            }
+        }
+
+        private void Zip_SaveProgress(object sender, Ionic.Zip.SaveProgressEventArgs e)
+        {
+            if (e.EventType == Ionic.Zip.ZipProgressEventType.Saving_BeforeWriteEntry)
+            {
+                progressBar1.Invoke(new MethodInvoker(delegate
+                {
+                    progressBar1.Maximum = e.EntriesTotal;
+                    progressBar1.Value = e.EntriesSaved + 1;
+                    progressBar1.Update();
+                }));
+            }
+        }
+
+        string kaynakKlasor = "", hedefKlasor = "";
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBoxDosyalar.SelectedItem != null)
+            {
+                string sil = listBoxDosyalar.SelectedItem.ToString();
+                listBoxDosyalar.Items.Remove(sil);
+                lblSilinen.Text = sil;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek istediğiniz dosyayı seçin");
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string metin = textBoxSil.Text;
+            var dosyalar = new DirectoryInfo(kaynakKlasor).GetFiles("*.*");
+            if (metin != string.Empty)
+            {
+                foreach (FileInfo dosya in dosyalar)
+                {
+                    string ilkHarf = metin.Substring(0, 1);
+                    string sonMetin = dosya.Name.Substring(dosya.Name.Length - 3, 3);
+                    if (metin == dosya.Name)
+                    {
+                        listBoxSilinenler.Items.Add(metin);
+                        textBoxSil.Text = "";
+                        lblSilinen.Text = metin;
+                        listBoxDosyalar.Items.Remove(dosya.Name);
+                    }
+                    else if (metin.Substring(metin.Length - 3, 3) == sonMetin && ilkHarf == "*")
+                    {
+                        listBoxSilinenler.Items.Add(dosya.Name);
+                        listBoxDosyalar.Items.Remove(dosya.Name);
+                    }
+                }
+            }
+        }
+
+        private void btnZiple_Click(object sender, EventArgs e)
+        {
+            var ziplenecekKlasor = textBoxHedef.Text;
+            var ziplenecekKlasor2 = textBoxHedef.Text + ("\\");
+            var ziplemeKonumu = ziplenecekKlasor2.Substring(0, ziplenecekKlasor2.LastIndexOf("\\") + 1);
+            var tarihBilgi = DateTime.Now.ToShortDateString().Replace(".", "-") + " " +  DateTime.Now.Hour.ToString() + "." + DateTime.Now.Minute.ToString();
+            var klasorAdi = tarihBilgi + " " + ziplenecekKlasor.Substring(ziplenecekKlasor.LastIndexOf("\\") + 1);
+            KlasorZipleme(ziplenecekKlasor, ziplemeKonumu + klasorAdi + ".zip", klasorAdi);          
+        }
+
+        private void KlasorZipleme(string kaynakKlasor, string ziplemeKonumu, string klasorAdi)
+        {
+            if (string.IsNullOrEmpty(textBoxHedef.Text))
+            {
+                MessageBox.Show("Please select your folder", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBoxHedef.Focus();
+                return;
+            }
+            string path = textBoxHedef.Text;
+            Thread thread = new Thread(t =>
+            {
+                using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile())
+                {
+                    zip.AddDirectory(kaynakKlasor);
+                    zip.SaveProgress += Zip_SaveProgress;
+                    zip.Save(ziplemeKonumu);
+                }
+                var dosyalar = new DirectoryInfo(hedefKlasor).GetFiles("*.*");
+                foreach (FileInfo dosya in dosyalar)
+                {
+                    if (dosya.Name != klasorAdi + ".zip")
+                    {
+                        dosya.Delete();
+                    }
+                }
+            })
+            { IsBackground = true };
+            thread.Start();
+        }
+
+        private void geriAlStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (lblSilinen.Text != string.Empty)
+            {
+                string geriAl = lblSilinen.Text;
+                listBoxDosyalar.Items.Add(geriAl);
+                lblSilinen.Text = string.Empty;
+            }
+        }
+    }
+}
